3f8cfd1e8e1591f1e393b749e833b3dbe07a1e65
Michael Hern <michaelhern@quickenloans.com>
2018-11-30 12:09:26 -0500

    Review

---
+++ ReviewExample/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using ReviewExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReviewExample.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DisplayName(User user1, User user2)
+        {
+           
+            return View(user2);
+        }
+
+    }
+}
---
+++ ReviewExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReviewExample.MvcApplication" Language="C#" %>
